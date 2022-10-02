15cdf5483fa099ca5897c45715686191796373a9
RafaelPS13 <114332367+RafaelPS13@users.noreply.github.com>
2022-10-01 21:54:06 -0300

    commit

---
+++ cafeteria/dados.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "35.198.20.233";
+$username = "root";
+$password = "admin123";
+$dbname = "clayteam_coffee";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname); 
+
+?>
